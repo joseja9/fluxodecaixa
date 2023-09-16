c5ae84ec55394ec5df67b1d717ca91ea33180952
Jose augusto Torres <joseja9184@gmail.com>
2023-09-15 22:10:11 -0300

    lancamento pesquisa (desc,dtfinal,dtinicial)

--- resources/views/lancamento/index.blade.php
+++ resources/views/lancamento/index.blade.php
@@ -1,81 +1,122 @@
 @extends('layouts.base')
 @section('content')
-    <h1><i class="bi bi-wallet2"></i>
+<h1>
+    <i class="bi bi-wallet2"></i>
+    - LANCAMENTOS
+    |
+    <a class="btn btn-primary" href="{{ route('lancamento.create') }}">
+        Novo Lançamento
+    </a>
+</h1>
 
-        - LANCAMENTOS
+{{-- alerts --}}
+@include('layouts.partials.alerts')
+{{-- /alerts --}}
+{{-- paginação --}}
+{!! $lancamentos->appends([
+                            'search'=>request()->get('search','')
+                        ])->links() !!}
+{{-- /paginação --}}
+{{-- pesquisa --}}
+<div class="row">
+    <form action="{{ route('lancamento.index') }}" method="get">
 
-        <a class="btn btn-primary"
-        href="{{ route ('lancamento.create') }}">
-            Novo Lançamento
+        <input class="form-control col-md-4" type="search" name="search" id="search"
+            placeholder="Digite o que deseja pesquisar..."
+            value="{{ old('search',request()->get('search')) }}">
+
+        {{-- data inicial --}}
+        <div class="col-md-3">
+            <label class="form-label" for="dt_inicial">
+                Data inicial
+            </label>
+            <input class="form-control"
+            type="date" name="dt_inicial" id="dt_inicial">
+        </div>
+        {{-- /data inicial --}}
+        {{-- data final --}}
+        <div class="col-md-3">
+            <label class="form-label" for="dt_final">
+                Data final
+            </label>
+            <input
+            class="form-control" type="date"
+            name="dt_final" id="dt_final">
+        </div>
+        {{-- /data final --}}
+
+
+        <input class="btn btn-success col-md-1" type="submit" value="Pesquisar">
+
+        @if(request()->get('search') !='')
+        <a class="btn btn-primary col-md-1"
+            href="{{ route('lancamento.index') }}">
+          Limpar
         </a>
+        @endif
 
-    </h1>
+    </form>
+</div>
+{{-- /pesquisa --}}
+<div class="table-responsive">
+    <table class="table table-striped  table-hover ">
+        <thead>
+            <tr>
+                <th>#</th>
+                <th>Vencimento</th>
+                <th>Tipo</th>
+                <th>Valor</th>
+                <th>Centro de Custo</th>
+                <th>Descrição</th>
+                <th>Usuário</th>
+                <th>Data do lançamento</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            @forelse ($lancamentos as $lancamento )
+            <tr>
+                <td scope="row" class="col-2">
+                    <div class="flex-column">
+                        {{-- ver anexo --}}
+                        {{-- {{ Storage::url('/anexos/'.$lancamento->anexo)}} --}}
+                        @if ($lancamento->anexo)
+                        <a class="btn btn-success" href="{{ Storage::url('/anexos/'.$lancamento->anexo)}}"
+                            target="_blank">
+                            <i class="bi bi-paperclip"></i>
+                        </a>
+                        @endif
 
-    {{-- alerts --}}
-    @include('layouts.partials.alerts')
-    {{-- /alerts --}}
-    {{-- paginação --}}
-          {!! $lancamentos->links() !!}
-    {{-- /paginação --}}
-
-    <div class="table-responsive">
-        <table class="table table-striped  table-hover ">
-            <thead>
-                <caption>LISTA DE</caption>
-                <tr>
-                    <th>#</th>
-                    <th>Data</th>
-                    <th>Tipo</th>
-                    <th>Valor</th>
-                    <th>Centro de Custo</th>
-                    <th>Descrição</th>
-                    <th>Usuário</th>
-                    <th>Data do Lançamento</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                @forelse ($lancamentos as $lancamento )
-
-
-                <tr>
-                    <td scope="row" class="col-2">
-                        <div class="flex-column">
-                            {{-- ver anexo --}}
-                            <a class="btn btn-success" href="{{url('/storage/anexos/'.$lancamento->anexo) }}" target="_blank">
-                                <i class="bi bi-paperclip"></i>
-                            </a>
-                            {{-- editar --}}
-                            <a class="btn btn-dark" href="#">
-                                <i class="bi bi-pencil-square"></i>
-                            </a>
-                            {{-- excluir --}}
-                            <button type="button" class="btn btn-danger" data-bs-toggle="modal"
-                                data-bs-target="#modalExcluir" data-identificacao="" data-url="">
-                                <i class="bi bi-trash"></i>
-                            </button>
-                        </div>
-                    </td>
-                    <td>{{$lancamento->vencimento->format('d/m/Y')}}</td>
-                    <td>{{ $lancamento->tipo->tipo }}</td>
-                    <td>{{ $lancamento->valor }}</td>
-                    <td>{{ $lancamento->centroCusto->centro_custo }}</td>
-                    <td>{{ $lancamento->descricao }}</td>
-                    <td>{{ $lancamento->usuario->name }}</td>
-                    <td>{{ $lancamento->created_at->format('d/m/Y \à\s H:i')}}
-
-                    </td>
-                </tr>
-                @empty
-                <tr>
-                    <td colspan="8">
-                        Nenhum registro retornado
-                    </td>
-                </tr>
-
-                @endforelse
-            </tbody>
-        </table>
-    </div>
+                        {{-- editar --}}
+                        <a class="btn btn-dark" href="#">
+                            <i class="bi bi-pencil-square"></i>
+                        </a>
+                        {{-- excluir --}}
+                        <button type="button" class="btn btn-danger" data-bs-toggle="modal"
+                            data-bs-target="#modalExcluir" data-identificacao="" data-url="">
+                            <i class="bi bi-trash"></i>
+                        </button>
+                    </div>
+                </td>
+                <td>{{ $lancamento->vencimento->format('d/m/Y') }}</td>
+                <td>{{ $lancamento->tipo->tipo }}</td>
+                <td>{{ $lancamento->valor }}</td>
+                <td>{{ $lancamento->centroCusto->centro_custo }}</td>
+                <td>{{ $lancamento->descricao }}</td>
+                <td>{{ $lancamento->usuario->name }}</td>
+                <td>
+                    {{ $lancamento->created_at->format('d/m/Y \a\s H:i') }}h
+                </td>
+            </tr>
+            @empty
+            <tr>
+                <td colspan="8">
+                    Nenhum registro retornado
+                </td>
+            </tr>
+            @endforelse
+        </tbody>
+    </table>
+</div>
 
 {{-- Modal Excluir --}}
 @include('layouts.partials.modalExcluir')
